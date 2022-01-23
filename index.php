44171c416c1b361d1f33e65b577a5057571d64c3
Joni Malaluan <jerniclou@gmail.com>
2022-01-23 22:25:34 +0800

    index with zip file

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+    Plugin Name: Chart plugin
+    Description: A plugin that displays chart by using shortcode
+    Version: 1.0
+    Author: Joni
+    Author URI: https://jonimalaluan.com
+*/
+
+
+/* This is a shortcode for displaying Radar Chart*/
+function spider_chart_shortcode( $atts ) {
+	ob_start();
+	?>
+    
+	<canvas id="myChart"></canvas>
+	<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+	<script>
+
+		var ctx = document.getElementById('myChart').getContext('2d');
+		var chart = new Chart(ctx, {
+			type: 'radar',
+			data: {
+				labels: ['First', 'Second', 'Third', 'Fourth', 'Fifth'],
+				datasets: [{
+					label: 'Resilient Assessment',
+					backgroundColor: 'rgb(255, 99, 132, 0.8)',
+					borderColor: 'rgb(255, 99, 132)',
+					data: [9, 9, 3, 8, 7]
+				}]
+			},
+
+			// Configuration options go here
+			options: {
+			}
+		});							
+	</script>
+
+
+	<?php 
+
+	return ob_get_clean();
+}
+add_shortcode( 'spider_chart-section', 'spider_chart_shortcode' );
